1d63ce9a17b1bf758b9265bcd39f0758bbc0f746
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2017-08-24 18:45:43 +0900

    Update

--- script/train.py
+++ script/train.py
@@ -9,11 +9,7 @@ tf.app.flags.DEFINE_string('directory', 'data', """Directory where to read *.tfr
 SIZE_WIDTH = 28
 SIZE_HEIGHT = 28
 
-def convert_to(dataset, name):
-    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
-    writer = tf.python_io.TFRecordWriter(filename)
-
-    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+def convert_to(dataset, filename):
     writer = tf.python_io.TFRecordWriter(filename)
 
     # pip3 install pillow
@@ -28,27 +24,31 @@ def convert_to(dataset, name):
 
         record = tf.train.Example(features=tf.train.Features(feature={
             'label' : tf.train.Feature(int64_list=tf.train.Int64List(value=[image_label])),
-            'image' : tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.tobytes()])),
-            'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
-            'width' : tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
-            'depth' : tf.train.Feature(int64_list=tf.train.Int64List(value=[3])),
+            'image' : tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.tobytes()]))
         }))
         writer.write(record.SerializeToString())
 
     writer.close()
 
-def inputs():
-    if not FLAGS.directory:
-        raise ValueError('Please supply a directory')
-
-    tfrecords_filename = 'train'
-    filename = os.path.join(FLAGS.directory, tfrecords_filename + '.tfrecords')
+def read_tensor_from(filename, input_mean=0, input_std=255):
     filename_queue = tf.train.string_input_producer([filename])
 
-    image, label = read_and_decode(filename_queue)
+    reader = tf.TFRecordReader()
+    key, value = reader.read(filename_queue)
+    features = tf.parse_single_example(value, features={
+            'label': tf.FixedLenFeature([], tf.int64),
+            'image': tf.FixedLenFeature([], tf.string),
+    })
+    image = tf.image.decode_jpeg(features['image'], channels=3)
+    float_image = tf.cast(image, tf.float32)
+    dims_expander = tf.expand_dims(float_image, 0)
 
-    return image, label
+    resized = tf.image.resize_bilinear(dims_expander, [SIZE_HEIGHT, SIZE_WIDTH])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
 
+    return result
 
 def main():
     argv = sys.argv
@@ -57,7 +57,11 @@ def main():
         ['./images/2.jpg', 1]
     ]
 
-    convert_to(dataset, 'train')
+    filename = os.path.join(FLAGS.directory, 'train.tfrecords')
+
+    convert_to(dataset, filename)
+    tensor = read_tensor_from('./images/input.jpg')
+    print(tensor)
 
 if __name__ == '__main__':
     main()
