f4dc9549b6d4915e9ac285c45783e9d5d9bfa7ce
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2017-08-24 17:29:11 +0900

    Update

--- script/train.py
+++ script/train.py
@@ -3,35 +3,61 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
-def main():
-    argv = sys.argv
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('directory', 'data', """Directory where to read *.tfrecords.""")
 
-    img_data = [
-        ['./images/1.jpg', 0],
-        ['./images/2.jpg', 1]
-    ]
+SIZE_WIDTH = 28
+SIZE_HEIGHT = 28
 
-    record_file = './test.tfrecords'
-    writer = tf.python_io.TFRecordWriter(record_file)
+def convert_to(dataset, name):
+    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+    writer = tf.python_io.TFRecordWriter(filename)
+
+    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+    writer = tf.python_io.TFRecordWriter(filename)
 
     # pip3 install pillow
     from PIL import Image
 
-    for img_datum in img_data:
-        img_file = img_datum[0]
-        img_label = img_datum[1]
+    for data in dataset:
+        image_path = data[0]
+        image_label = data[1]
 
-        img = Image.open(img_file)
-        width, height = img.size
+        image = Image.open(image_path).convert("RGB").resize((SIZE_WIDTH, SIZE_HEIGHT))
+        width, height = image.size
 
         record = tf.train.Example(features=tf.train.Features(feature={
-            'label' : tf.train.Feature(int64_list=tf.train.Int64List(value=[img_label])),
-            'image' : tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tobytes()])),
+            'label' : tf.train.Feature(int64_list=tf.train.Int64List(value=[image_label])),
+            'image' : tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.tobytes()])),
             'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
             'width' : tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
             'depth' : tf.train.Feature(int64_list=tf.train.Int64List(value=[3])),
         }))
         writer.write(record.SerializeToString())
+
+    writer.close()
+
+def inputs():
+    if not FLAGS.directory:
+        raise ValueError('Please supply a directory')
+
+    tfrecords_filename = 'train'
+    filename = os.path.join(FLAGS.directory, tfrecords_filename + '.tfrecords')
+    filename_queue = tf.train.string_input_producer([filename])
+
+    image, label = read_and_decode(filename_queue)
+
+    return image, label
+
+
+def main():
+    argv = sys.argv
+    dataset = [
+        ['./images/1.jpg', 0],
+        ['./images/2.jpg', 1]
+    ]
+
+    convert_to(dataset, 'train')
 
 if __name__ == '__main__':
     main()
